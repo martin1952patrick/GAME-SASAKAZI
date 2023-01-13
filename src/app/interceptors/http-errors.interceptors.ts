538153ec114cdbd895c27ad7f0ee065f709e770f
martin1952patrick <martinpati1952@gmail.com>
2023-01-13 11:38:53 +0300

    add src

---
+++ src/app/interceptors/http-errors.interceptors.ts
@@ -0,0 +1,22 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { catchError } from "rxjs/operators";
+import { Injectable } from "@angular/core";
+import { Observable, throwError as observableThrowError } from "rxjs";
+
+@Injectable()
+export class HttpErrorsInterceptor implements HttpInterceptor {
+    constructor(){}
+
+    intercept (
+        req: HttpRequest<any>,
+        next: HttpHandler
+    ): Observable<HttpEvent<any>> {
+        return next.handle(req).pipe(
+            catchError((err) => {
+                console.log(err);
+                return observableThrowError(err);
+            })
+        );
+    }
+}
+
